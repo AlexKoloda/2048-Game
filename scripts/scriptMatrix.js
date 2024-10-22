29b954b21cda7c4e0f9bf950e525a858e9414fc1
Alex Koloda <xeniumsonyz@gmail.com>
2024-10-22 15:14:39 +0300

    Добалена функция генерации новый ячеек

--- scripts/scriptMatrix.js
+++ scripts/scriptMatrix.js
@@ -1,16 +1,41 @@
 // TODO Придумать как реализовать анимации перехода между состояниями
 
+// Идеи для рандомайзера
+
+/* 
+2) в функции generateCell проверяем, если наша рандомная ячейка с 0, вставляем в нее число, 
+которое получаем из ф-и из пункта 3, а если число не 0, она вызывает себя
+
+3) Написать функцию которая возращает 2 или 4 с помощью рандомайзера
+
+4) в дополнению к пункту 2, после генерации ячейки, стоит проверить, если вообще дальше смысл играть */
+
 let gameContainer = document.querySelector(".game__container_iner");
 const buttonNewGame = document.querySelector(".header_button_new");
+let isFirstCall = true;
 
 let gameMatrix = [
   [0, 0, 0, 0],
-  [0, 2, 0, 0],
+  [4, 0, 2, 0],
   [0, 0, 0, 0],
-  [0, 0, 2, 0],
+  [0, 0, 0, 0],
 ];
 
-function createGameField(arr) {
+buttonNewGame.addEventListener("click", () => {
+  createNewGeme(gameMatrix);
+});
+
+function createNewGeme(arr) {
+  createGameField(gameMatrix);
+}
+
+function createGameField(arr, isFirstCall = false) {
+  if (isFirstCall) {
+
+    generateCell(gameMatrix);
+    generateCell(gameMatrix);
+  }
+
   gameContainer.innerHTML = "";
 
   arr.flat().forEach((tailValue) => {
@@ -22,7 +47,6 @@ function createGameField(arr) {
       tail.className = "game__tail";
       cell.append(tail);
     } else {
-      console.log(tailValue);
       tail.className = `game__tail tail_${tailValue}`;
       cell.append(tail);
     }
@@ -30,10 +54,53 @@ function createGameField(arr) {
   });
 }
 
-buttonNewGame.addEventListener("click", () => {
-  createNewGeme(gameMatrix);
-});
+function generateCell(arr) {                        // НЕ РАБОАТЕТ выбор ячейки! РАЗОБРАТЬСЯ!
+  arr = gameMatrix;
+  const colIndex = Math.floor(Math.random() * 3);
+  const rowIndex = Math.floor(Math.random() * 3);
 
-function createNewGeme(arr) {}
+  if (arr[colIndex][rowIndex] === 0) {
+    const tailValue = generateTailValue();
+    arr[colIndex][rowIndex] = tailValue;
+  } else {
+    generateCell(arr);
+  }
+}
+
+function generateTailValue() {
+  let randomNumber = Math.random().toFixed(2);
+
+  if (randomNumber < 0.1) {
+    return 4;
+  } else {
+    return 2;
+  }
+}
+
+function createNewGeme(arr) {
+  if (isFirstCall) {
+    isFirstCall = false;
+    generateCell();
+    generateCell();
+  }
+
+  createGameField(gameMatrix);
+}
 
 createGameField(gameMatrix);
+
+/* 
+createGameField( gameMatrix, isFirstCall = false) */
+
+/* func example(arr) {
+  const colIndex= math.blablabla;
+ const rowIndex = blablabla;
+ 
+    if (arr[colIndex][rowIndex] === 0) {
+   const numberToFill = random2or4();
+ 
+    arr[colIndex][rowIndex] = number; (или через inner) хз как у тебя там правильно по логике
+ 
+ 
+ } else {example(arr)}
+ } */
