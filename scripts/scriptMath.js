933df40d89455fea26e5ff785c59d0f150d6a4a7
Alex Koloda <xeniumsonyz@gmail.com>
2024-10-23 14:31:56 +0300

    "Исправление анимаций "

--- scripts/scriptMath.js
+++ scripts/scriptMath.js
@@ -1,8 +1,9 @@
 
 let scoreDisplay = document.querySelector(".score__value");
 let score = '0';
+
 let bestScoreDisplay = document.querySelector(".best__value");
-scoreDisplay.textContent = score;
+scoreDisplay.textContent = makeScoreCounter();
 bestScoreDisplay.innerHTML += score;
 
 
@@ -14,3 +15,8 @@ function sumTailValue(a, b) {
     return  a + b;
 }
 
+function makeScoreCounter() {
+  
+  score = sumTailValue();
+  console.log(score);
+}
